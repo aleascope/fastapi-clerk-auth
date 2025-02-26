1555f884a3722d212cbcf7623a03900d54f098c7
m.zaradzki <mathieu.zaradzki@gmail.com>
2025-02-26 14:14:33 +0100

    Update auth.py

--- utils/auth.py
+++ utils/auth.py
@@ -4,13 +4,13 @@ from fastapi import Request, Query, Depends, Response, status
 from fastapi_clerk_auth import ClerkConfig, ClerkHTTPBearer, HTTPAuthorizationCredentials
 
 # Imports from this repo
-from config import config
+from config import settings
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 # Create a router and apply authentication globally
 
-clerk_config = ClerkConfig(jwks_url=f"https://{config.CLERK_DOMAIN}/.well-known/jwks.json")
+clerk_config = ClerkConfig(jwks_url=f"https://{settings.CLERK_DOMAIN}/.well-known/jwks.json")
 clerk_auth_guard = ClerkHTTPBearer(config=clerk_config)
 
 async def clerk_auth(credentials: HTTPAuthorizationCredentials | None = Depends(clerk_auth_guard)):
