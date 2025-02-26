dbc188ca4eb0970eb438305c4e9ca9663614fef0
m.zaradzki <mathieu.zaradzki@gmail.com>
2025-02-26 13:37:45 +0100

    Create protected.py

---
+++ protected.py
@@ -0,0 +1,34 @@
+# Imports from public packages
+import logging
+from fastapi import FastAPI, APIRouter, Request, Query, HTTPException, Depends, Response, status
+from fastapi_clerk_auth import ClerkConfig, ClerkHTTPBearer, HTTPAuthorizationCredentials
+
+# Imports from this repo
+from workers.config import config
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# Create a router and apply authentication globally
+
+clerk_config = ClerkConfig(jwks_url=f"https://{config.CLERK_DOMAIN}/.well-known/jwks.json")
+clerk_auth_guard = ClerkHTTPBearer(config=clerk_config)
+
+async def clerk_auth(credentials: HTTPAuthorizationCredentials | None = Depends(clerk_auth_guard)):
+    """Dependency to enforce authentication on protected routes."""
+    token = credentials.credentials
+    logging.info(f"Received Bearer token: {token}")
+    return credentials
+
+protected = APIRouter(
+    tags=["Protected API"],
+    dependencies=[Depends(clerk_auth)]
+)
+
+
+@protected.post("/private/func")
+def func(msg: str):
+    """
+    Example:
+        msg = 'a dummy payload'
+    """
+    return {"message": msg}
