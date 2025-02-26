3850fce4ccefadf694d4b661b16a6cdb993a085a
m.zaradzki <mathieu.zaradzki@gmail.com>
2025-02-26 13:25:03 +0100

    Create config.py

---
+++ app/config.py
@@ -0,0 +1,18 @@
+from os import environ as ENV
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    APP_TAG: str = ENV.get('APP_TAG')
+    CLERK_PK: str = ENV.get('CLERK_PK')
+    CLERK_DOMAIN: str = ENV.get('CLERK_DOMAIN')
+        
+    
+class FileSettings(Settings):
+    class Config:
+        env_file = "workers/.env"
+
+if False:
+    config = FileSettings()  # Load variables from .env file
+else:
+    config = Settings()  # Use os env variables only
