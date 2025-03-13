6303f17a801c43f48840c54680d405460dc288f0
m.zaradzki <mathieu.zaradzki@gmail.com>
2025-03-13 05:15:00 +0100

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -4,8 +4,12 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     APP_TAG: str = ENV.get('APP_TAG')
+    # Settings if you intend to use Clerk OAuth
     CLERK_PK: str = ENV.get('CLERK_PK') # Your Clerk Publishable Key
     CLERK_DOMAIN: str = ENV.get('CLERK_DOMAIN') # Your Clerk Domain e.g. xxx.clerk.accounts.dev
+    # Settings if you intend to use GCP OAuth
+    GC_PROJECT: str = ENV.get('GC_PROJECT')
+    GC_OAUTH_AUDIENCE: str = ENV.get('GC_OAUTH_AUDIENCE')
         
     
 class FileSettings(Settings):
